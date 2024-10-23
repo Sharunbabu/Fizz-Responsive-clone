b83399b1e209835d1ce6f0bc800fbd5415d4a588
SHARUN BABU <133116021+Sharunbabu@users.noreply.github.com>
2024-10-23 16:46:37 +0530

    Fizz first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function rotate(){
+    let element = document.getElementById("rotateImage");
+window.addEventListener('scroll', function () {
+    let variable = window.scrollY * 0.35;
+    element.style.transform = `rotate(${variable}deg)`; // Use backticks for template literals
+});
+}
